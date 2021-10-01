61b1f51593321c03075640108c1e5d83e58a7ef1
skleinma <52725279+skleinma@users.noreply.github.com>
2021-09-30 21:39:21 -0700

    Delete 2_cookie_counter.js

--- 2_cookie_counter.js
+++
@@ -1,8 +0,0 @@
-let cCounter = 0;
-// Set a Cookie function
-function setCookie(cName, cValue) {
-        document.cookie = cName + "=" + cValue + "; path=/";
-}
-// Apply setCookie
-setCookie('cCounter', cCounter);
-console.log(document.cookie);
